b1e2466f060c8de5b8070914e5954adc5b4592c9
Evan Friedenberg <evan.friedenberg@gmail.com>
2024-04-14 13:16:18 -0400

    Preparing for v10

---
+++ src/Evergreen/V10/Types.elm
@@ -0,0 +1,68 @@
+module Evergreen.V10.Types exposing (..)
+
+import Dict
+import Element
+import Lamdera
+
+
+type alias CardOptions =
+    List Int
+
+
+type alias Game =
+    { code : String
+    , cardOptions : CardOptions
+    , playedCards : Dict.Dict Lamdera.ClientId (Maybe Int)
+    }
+
+
+type alias FrontendModel =
+    { game : Maybe Game
+    , selectedCard : Maybe Int
+    , enteredGameCode : String
+    , cardOptions : CardOptions
+    , hideStats : Bool
+    , device : Element.Device
+    , key : Maybe Lamdera.Key
+    }
+
+
+type alias BackendModel =
+    Dict.Dict String Game
+
+
+type FrontendMsg
+    = NoOpFrontendMsg
+    | GameCreated
+    | CardSelected Game Int
+    | GameCodeEntered String
+    | RequestGame
+    | LeftGame
+    | ToggleStats
+    | ResetGame Game
+    | ChooseCardOptions CardOptions
+    | CopyCodeToClipboard Game
+    | GotWindowDimensions Int Int
+
+
+type ToBackend
+    = NoOpToBackend
+    | CreateGame CardOptions
+    | JoinGame String
+    | LeaveGame
+    | UpdatePlayerCard String Int
+    | ResetGameCards String
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | ClientConnected Lamdera.SessionId Lamdera.ClientId
+    | ClientDisconnected Lamdera.SessionId Lamdera.ClientId
+    | CodeCreated Lamdera.ClientId CardOptions String
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | CreatedGameReceived (Maybe Game)
+    | GameReceived (Maybe Game)
+    | GameReset (Maybe Game)
